8246428bd76dc19dc2b8560ebd179cb2a1bf4d9a
biduldoudel <nath.ferra@hotmail.com>
2016-11-16 15:48:07 +0100

    tab added

--- userFiles/ctrl/groups_ctrl/gr2/path/path_planning_gr2.cc
+++ userFiles/ctrl/groups_ctrl/gr2/path/path_planning_gr2.cc
@@ -6,6 +6,50 @@
 
 NAMESPACE_INIT(ctrlGr2);
 
+static double centre_obstacle[][] = { 
+	0.04,	0.16,
+	0.34,	0.16,
+	0.34,	0.54,
+	-0.34,	0.54,
+	-0.34,	0.24,
+	-0.64,	0.24,
+	-0.64,	-0.24,
+	-0.34,	-0.24,
+	-0.34,	-0.54,
+	0.34,	-0.54,
+	0.34,	-0.16,
+	0.04,	-0.16
+}
+
+static double wall_1[][] = {
+	0.64,	1.36,
+	0.64,	0.86,
+	0.34,	0.86,
+	0.34,	1.36,
+}
+
+static double wall_2[][] = {
+	-0.86,	0.99,
+	-0.36,	0.99,
+	-0.36,	0.69,
+	-0.86,	0.69,
+}
+
+static double wall_3[][] = {
+	-0.86, -0.69,
+	-0.36, -0.69,
+	-0.36, -0.99,
+	-0.86, -0.99,
+}
+
+static_double wall_4[][] = {
+	0.34, -1.36,
+	0.34, -0.86,
+	0.64, -0.86,
+	0.64, -1.36,
+
+}
+
 /*! \brief initialize the path-planning algorithm (memory allocated)
  * 
  * \param[in,out] path path-planning main structure
