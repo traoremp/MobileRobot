5102e34630d64c7320229f75dacd1d8bfcc03b03
traoremp <traoremp@vt.edu>
2016-11-16 15:35:52 +0100

    path planning clsses done

--- userFiles/ctrl/groups_ctrl/gr2/path/path_planning_gr2.h
+++ userFiles/ctrl/groups_ctrl/gr2/path/path_planning_gr2.h
@@ -11,7 +11,30 @@
 #include "CtrlStruct_gr2.h"
 #include <memory>
 #include <vector>
+#include <map>
+#include <set>
 NAMESPACE_INIT(ctrlGr2);
+class Map_Element {
+	public: 
+		Map_Element(double, double);
+	private: 
+		double position_x_;
+		double position_y_;
+}
+class Obstacle{
+	public: 
+		Obstacle(std::unique_ptr<std::List> points);
+	private : 
+		std::unique_ptr<std::List<std::unique_ptr<Map_Element>>> points_; 
+}
+class TreeNode{
+	public:
+		TreeNode();
+	private:
+		std::unique_ptr<TreeNode> parent_;
+		vector<std::unique_ptr<std::pair<int, TreeNode>>> children_;
+		Map_Element position_;
+}
 class PathTree{
 	public:
 		PathTree();
@@ -20,16 +43,10 @@ class PathTree{
 		std::unique_ptr<TreeNode> root_;
 		std::unique_ptr<TreeNode> target_Node_;
 }
-class TreeNode{
-	public:
-		TreeNode();
-	private:
-		std::unique_ptr<TreeNode> parent_;
-		vector<std::unique_ptr<std::pair<int, TreeNode>>> children_;
-}
 /// path-planning main structure
 struct PathPlanning
 {
+	std::unique_ptr<std::set<Obstacle>> obstacles_;
 	int dummy_variable; ///< put your own variable, this is just an example without purpose
 };
 
